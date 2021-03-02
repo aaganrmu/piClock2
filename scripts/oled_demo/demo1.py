09b25cc7475194f389522d9d435b853036b8d860
Elmar Wiedemeijer <elmar.wiedemeijer@new10.com>
2021-03-02 23:23:49 +0100

    fix: added address to oled

--- scripts/oled_demo/demo1.py
+++ scripts/oled_demo/demo1.py
@@ -9,7 +9,7 @@ HEIGHT = 32
 
 oled_reset = digitalio.DigitalInOut(board.D4)
 i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=oled_reset)
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c, reset=oled_reset)
 
 oled.fill(0)
 oled.show()
