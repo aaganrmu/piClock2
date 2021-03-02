d20328de70c69555305e46920ec8a5720d14d9ff
Elmar Wiedemeijer <elmar.wiedemeijer@new10.com>
2021-03-02 23:42:24 +0100

    fix: try SPI

--- scripts/oled_demo/demo1.py
+++ scripts/oled_demo/demo1.py
@@ -1,4 +1,5 @@
 import board
+import busio
 import digitalio
 import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont
@@ -7,10 +8,14 @@ from PIL import Image, ImageDraw, ImageFont
 WIDTH = 128
 HEIGHT = 32
 
-oled_reset = digitalio.DigitalInOut(board.D4)
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c, reset=oled_reset)
-
+# oled_reset = digitalio.DigitalInOut(board.D4)
+# i2c = board.I2C()
+# oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c, reset=oled_reset)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+reset_pin = digitalio.DigitalInOut(board.D4)
+cs_pin = digitalio.DigitalInOut(board.D5)
+dc_pin = digitalio.DigitalInOut(board.D6)
+oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, dc_pin, reset_pin, cs_pin)
 
 image = Image.new("1", (oled.width, oled.height))
 
