0f53873b34ba8b22684ebd6a24b2c1fa2187ef54
mauganra99 <mauganra99@hotmail.com>
2021-07-07 18:24:40 +0100

    Added shutdown button

--- scripts/gpio/blinker3.py
+++ scripts/gpio/blinker3.py
@@ -1,20 +1,24 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+import subprocess
+
 GPIO.setmode(GPIO.BOARD)
 
 led_pin = 12
 button_pin = 16
+quit_button_pin = 18
 
 class gpio_in:
     def __init__(self, pin):
         self._pin = pin
         GPIO.setup(self._pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    @property
     def state(self):
-        return GPIO.input(self._pin)
+        return GPIO.input(self._pin) == 0
 
- 
+
 class gpio_out:
     def __init__(self, pin):
         self._pin = pin
@@ -38,17 +42,24 @@ class gpio_out:
 
 led = gpio_out(led_pin)
 button = gpio_in(button_pin)
-
+quit_button = gpio_in(quit_button_pin)
+shutting_down = False
 timer = 0.003
+previous_state = False
 try:
     while True:
-        
-        if button.state():
-            led.state = True
-            print('pushed')
-        else:
-            led.state = False
-            print('not')
+        current_state = button.state
+        if current_state != previous_state:
+            led.state = current_state
+            previous_state = current_state
+            if current_state:
+                print("now on")
+            else:
+                print("now off")
+        if quit_button.state and not shutting_down:
+            shutting_down = True
+            subprocess.check_output("sudo shutdown +0", shell=True)
+
 finally:
     GPIO.cleanup()
 
